886b5ef1334e645e33b74bdf30570d5a70af638e
entrar <entrar@PC-aula11>
2017-10-16 12:06:23 +0100

    Version 3

---
+++ src/javaapplication2/CalculaHistogram.java
@@ -0,0 +1,13 @@
+package javaapplication2;
+
+public class CalculaHistogram {
+      
+    public static <T> Histogram <T> computeHistogram (T[] vector){
+        
+        Histogram <T> histo = new Histogram<> ();
+        for (T key : vector) {
+            histo.increment(key);
+        }
+        return histo;
+    }    
+}
