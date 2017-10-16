43efb9de71536b44c39f2ec16b61b8244c35bf13
entrar <entrar@PC-aula11>
2017-10-16 11:33:25 +0100

    Version 1 solo main

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.HashMap;
+
+public class JavaApplication2 {
+
+    public static void main(String[] args) {
+
+         
+        int[] vector = {1, 3, 5, 7, 9, 1, 5, 4, 3, 9,100,-4};
+        
+        Histogram mihisto = new Histogram(vector);
+        HashMap<Integer, Integer> histogram = mihisto.getHisto();
+        
+        for (Integer key : histogram.keySet()) {
+            System.out.println(key + " --> " + histogram.get(key));
+        }
+    
+    
+    }
+    
+}
